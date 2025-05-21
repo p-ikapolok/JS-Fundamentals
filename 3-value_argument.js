95d6ad4b4921dfcee91c94f5613d89e8c92915cc
p-ikapolok <ikapolokpeter480@gmail.com>
2025-05-21 16:46:39 -0700

    Fix: Remove .length and handle first argument only

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-// Get the first argument without using .length
 const firstArg = process.argv[2];
 
-// Print the first argument or "No argument" if none exists
-console.log(firstArg === undefined ? "No argument" : firstArg);
+if (firstArg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(firstArg);
+}
