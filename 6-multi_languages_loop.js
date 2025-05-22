f062a9d6aed82779c3a5b97fbdfb1a59d1ca8ab7
p-ikapolok <ikapolokpeter480@gmail.com>
2025-05-21 17:12:33 -0700

    multi languages loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Array of language strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through array and print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
